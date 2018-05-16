e8f5d94b7ae886d3f18811d515dd084597b0518e
Leslie Emery <emeryl@uw.edu>
2018-05-15 17:08:34 -0700

    Change settings as instructed

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,7 @@
 from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='wyqnr0mgkn_c4!4g)mii54_0ja1z1iy=ki_=x1tvcqaxl(@=c+')
+
+DEBUG = env.bool('DJANGO_DEBUG', True)
