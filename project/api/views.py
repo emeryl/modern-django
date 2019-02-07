3a0bdd980a915a1ed57ed78f22f517912ae291b7
Leslie Emery <emeryl@uw.edu>
2019-02-07 11:17:41 -0800

    Add UserViewSet api view
    Step 8-4 in part 2.

--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import viewsets
+
+from project.api.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """API endpoint allowing viewing and editing of Users."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
